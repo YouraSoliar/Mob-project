493f28f8c662193590a8f2387b0be63811199768
Yurii Soliar <us1585707@gmail.com>
2021-05-05 21:48:15 +0300

    Task page

--- app/src/main/java/com/example/dodo/MainActivity.java
+++ app/src/main/java/com/example/dodo/MainActivity.java
@@ -1,14 +1,106 @@
 package com.example.dodo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.dodo.Adapter.ToDoAdapter;
+import com.example.dodo.Model.ToDoModel;
+import com.example.dodo.Utils.DataBaseHandler;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements DialogCloseListener {
+
+    private RecyclerView tasksRecyclerView;
+    private ToDoAdapter taskAdapter;
+    private FloatingActionButton fab;
+
+    private List<ToDoModel> taskList;
+    private DataBaseHandler db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        db = new DataBaseHandler(this);
+        db.openDatabase();
+
+        taskList = new ArrayList<>();
+        tasksRecyclerView = findViewById(R.id.taskRecyclerView);
+        tasksRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        taskAdapter = new ToDoAdapter(db,this);
+        tasksRecyclerView.setAdapter(taskAdapter);
+
+        fab = findViewById(R.id.fab);
+
+        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(new RecyclerItemTouchHelper(taskAdapter));
+        itemTouchHelper.attachToRecyclerView(tasksRecyclerView);
+
+       taskList = db.getAllTasks();
+       Collections.reverse(taskList);
+       taskAdapter.setTask(taskList);
+
+       fab.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               AddNewTask.newInstance().show(getSupportFragmentManager(), AddNewTask.TAG);
+           }
+       });
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater ();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.buyList) {
+            Intent buyListIntent = new Intent(this, BuyList.class);
+            startActivity(buyListIntent);
+            return true;
+        } else if (id == R.id.calendar) {
+            Intent calendarIntent = new Intent(this, Calendar.class);
+            startActivity(calendarIntent);
+            return true;
+        } else if (id == R.id.taskList) {
+            Intent taskListIntent = new Intent(this, TaskList.class);
+            startActivity(taskListIntent);
+            return true;
+        } else if (id == R.id.reminder) {
+            Intent reminderIntent = new Intent(this, Reminder.class);
+            startActivity(reminderIntent);
+            return true;
+        } else if (id == R.id.tasks) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void handleDialogClose(DialogInterface dialog){
+        taskList = db.getAllTasks();
+        Collections.reverse(taskList);
+        taskAdapter.setTask(taskList);
+        taskAdapter.notifyDataSetChanged();
+    }
+
 }
---
+++ app/src/main/java/com/example/dodo/SplashActivity.java
@@ -0,0 +1,31 @@
+package com.example.dodo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
+
+public class SplashActivity extends AppCompatActivity {
+    ImageView logoImageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        getSupportActionBar().hide();
+        logoImageView = findViewById(R.id.logoImageView);
+        logoImageView.animate().alpha(1).setDuration(2000);
+
+
+        final Intent i = new Intent(SplashActivity.this, MainActivity.class);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(i);
+                finish();
+            }
+        }, 2000);
+    }
+}
